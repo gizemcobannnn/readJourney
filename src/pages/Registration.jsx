830460fdf36e960502d389556d659dbc4d81492b
gizemcoban <gizem.coban.r@gmail.com>
2025-07-06 16:12:56 +0200

    yup wiht formik

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,23 +1,41 @@
 import React from 'react'
 import {Formik,Form,Field,ErrorMessage} from "formik"
+import * as Yup from "yup";
 import frame from "../assets/screen-frame.svg"
 import wallpaper from "../assets/wallpaper.svg"
+
 export default function Registration() {
   const handleSubmit=()=>{
 
   }
+  const validationRegister=Yup.object({
+    name:Yup.string()
+    .min(6, "It has to be at least 6 characters")
+    .max(20, "It can be at most 20 characters").required("You have to enter your name"),
+    email:Yup.string().email()
+    .min(6, "It has to be at least 6 characters")
+    .max(20, "It can be at most 20 characters").required("You have to enter your email"),
+    password: Yup.string()
+    .min(6, "It has to be at least 6 characters")
+    .max(20, "It can be at most 20 characters")
+    .required("You have to enter your password")  })
+
   return (
-    <div className='grid grid-cols-2 gap-5'>
+    <div className='grid grid-cols-2 gap-35 p-10'>
         <div className='flex flex-col items-start justify-start p-5 relative'>
             <p className='text-start absolute top-0 left-0 mb-10'>READ JOURNEY</p>
             <div className='mt-20 flex gap-10 flex-col'>
-                <h1 className='text-start'>Expand your mind, reading <span>a book</span></h1>
-                <Formik initialValues={{name:"",email:"",password:""}} onSubmit={handleSubmit}>
+                <h1 className='text-start text-5xl font-semibold'>Expand your mind, reading <span>a book</span></h1>
+                <Formik initialValues={{name:"",email:"",password:""}} onSubmit={handleSubmit} validationSchema={validationRegister}>
                     <Form className='flex flex-col gap-5'>
                         <Field name="name" type="text" placeholder="Name" />
+                        <ErrorMessage name="name" component="div" className='errormessage' />
                         <Field name="email" type="email" placeholder="Email"/>
+                        <ErrorMessage name="name" component="div" className='errormessage' />
                         <Field name="password" type="password" placeholder="Password"/>
-                        <div className='flex flex-row justify-between mt-7'><button type="submit" >Register</button>
+                        <ErrorMessage name="name" component="div" className='errormessage' />
+                        <div className='flex flex-row justify-between mt-7'>
+                        <button type="submit" className='authbutton'>Register</button>
                         <button type="button">Already have an account ?</button></div>
                     </Form>
                 </Formik>

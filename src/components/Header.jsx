a69f779bfa31c4a61ff9e736b2edbff3bc48ae6d
gizemcoban <gizem.coban.r@gmail.com>
2025-08-16 00:10:18 +0200

    home page added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,15 @@ import { NavLink } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div className='fixed z-50 top-0 left-0 w-full p-2 text-white flex flex-row justify-between mb-2'>
-        <h1>READ JOURNEY</h1>
-        <nav className='flex flex-row gap-5'>
-           <NavLink  to="/home" className="navlink">Home</NavLink> 
+    <div className='fixed z-50 items-center left-0 w-full p-3 text-white flex flex-row justify-between mb-2 bg-part'>
+        <h1 className='text-center'>READ JOURNEY</h1>
+        <nav className='flex flex-row  gap-5'>
+           <NavLink  to="/recommendation" className="navlink">Home</NavLink> 
            <NavLink to="/mylibrary" className="navlink">My Library</NavLink> 
         </nav>
-        <div className='flex flex-row gap-2'>
+        <div className='flex flex-row gap-2 items-center '>
             <p className='border border-slate-400 text-white p-3'>img</p>
-            <p>name</p>
+            <p className='text-center'>name</p>
             <button type="button" className='border border-slate-400 text-white p-3'>Logout</button>
         </div>
     </div>
---
+++ src/pages/Recommendation.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useNavigate } from 'react-router';
+const Recommendation = () => {
+    let navigate = useNavigate();
+    const handleLibrary = () => {
+        navigate("/mylibrary");
+        console.log("Navigating to My Library");
+    }
+  return (
+    <div className='flex  flex-row min-h-screen overflow-hidden justify-between gap-5 p-4 bg-part mt-10'>
+        <div className='left-side flex flex-col gap-5 w-1/4 text-start'>
+            <p>Filters: </p>
+            <input type="text" name="title" id="title" placeholder='Book Title: ' className='bg-part' />
+            <input type="text" name="author" id="author" placeholder='The Author: ' className='bg-part' />
+            <button>To apply</button>
+
+            <h2 className='text-xl'>Start your workout</h2>
+            <p>Create a personal library: add the books you intend to read </p>
+            <p>Create your first workout: define a goal, choose a period, start training.</p>
+
+            <div className='flex flex-row justify-between'>
+                <button type="button" onClick={()=>handleLibrary()} className='text-start'>My library</button>
+            <button>ok</button>
+            </div>
+        </div>
+
+        <div className='right-side'></div>
+    </div>
+  )
+}
+
+export default Recommendation

c647ef948625ccbe5d6c0f7df3e5c777625201e4
gizemcoban <gizem.coban.r@gmail.com>
2025-08-17 15:59:42 +0200

    authslıce operatıons

--- src/redux/data/authSlice.js
+++ src/redux/data/authSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit"
-
+import { loginUser, registerUser, logoutUser, currentUser, currentRefresh } from "./authOps";
 
 const initialState = {
     name: "auth",
@@ -14,19 +14,79 @@ export const authSlice = createSlice({
     initialState: initialState,
     reducers:{},
     extraReducers:(builder)=>{
-        builder.addCase("auth/login/fulfilled", (state, action) => {
+        builder.addCase(loginUser.fulfilled, (state, action) => {
             state.email = action.payload.email;
             state.token = action.payload.token;
             state.isAuthenticated = true;
             state.error = null;
         })
-        .addCase("auth/login/rejected", (state, action) => {
+        .addCase(loginUser.pending, (state, action) => {
             state.error = action.payload;
             state.isAuthenticated = false;
-        }).addCase("auth/logout/fulfilled", (state) => {
+        }).addCase(loginUser.rejected, (state) => {
             state.email = "";
             state.token = "";
             state.isAuthenticated = false;
+            state.error = null;
+        }).addCase(registerUser.fulfilled, (state,action)=>{
+            state.email = action.payload.email;
+            state.token = action.payload.token;
+            state.isAuthenticated = true;
+            state.error = null;
+        }).addCase(registerUser.pending, (state,action)=>{
+            state.email = action.payload.email;
+            state.token = action.payload.token;
+            state.isAuthenticated = true;
+            state.error = null;
+        }).addCase(registerUser.rejected, (state,action)=>{
+            state.email = action.payload.email;
+            state.token = action.payload.token;
+            state.isAuthenticated = true;
+            state.error = null;
+        }).addCase(logoutUser.fulfilled, (state,action)=>{
+            state.email = action.payload.email;
+            state.token = action.payload.token;
+            state.isAuthenticated = true;
+            state.error = null;
+        }).addCase(logoutUser.pending, (state,action)=>{
+            state.email = action.payload.email;
+            state.token = action.payload.token;
+            state.isAuthenticated = true;
+            state.error = null;
+        }).addCase(logoutUser.rejected, (state,action)=>{
+            state.email = action.payload.email;
+            state.token = action.payload.token;
+            state.isAuthenticated = true;
+            state.error = null;
+        }).addCase(currentRefresh.fulfilled, (state,action)=>{
+            state.email = action.payload.email;
+            state.token = action.payload.token;
+            state.isAuthenticated = true;
+            state.error = null;
+        }).addCase(currentRefresh.pending, (state,action)=>{
+            state.email = action.payload.email;
+            state.token = action.payload.token;
+            state.isAuthenticated = true;
+            state.error = null;
+        }).addCase(currentRefresh.rejected, (state,action)=>{
+            state.email = action.payload.email;
+            state.token = action.payload.token;
+            state.isAuthenticated = true;
+            state.error = null;
+        }).addCase(currentUser.fulfilled, (state,action)=>{
+            state.email = action.payload.email;
+            state.token = action.payload.token;
+            state.isAuthenticated = true;
+            state.error = null;
+        }).addCase(currentUser.pending, (state,action)=>{
+            state.email = action.payload.email;
+            state.token = action.payload.token;
+            state.isAuthenticated = true;
+            state.error = null;
+        }).addCase(currentUser.rejected, (state,action)=>{
+            state.email = action.payload.email;
+            state.token = action.payload.token;
+            state.isAuthenticated = true;
             state.error = null;
         })
     }

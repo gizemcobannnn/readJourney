b4742645d51e833ca96253163a09f40085179e20
gizemcoban <gizem.coban.r@gmail.com>
2025-08-17 17:20:24 +0200

    justifying recommended page

--- src/pages/Recommendation.jsx
+++ src/pages/Recommendation.jsx
@@ -7,9 +7,9 @@ const Recommendation = () => {
         console.log("Navigating to My Library");
     }
   return (
-    <div className='flex  flex-row min-h-screen overflow-hidden justify-between gap-5 p-4 bg-part mt-10'>
-        <div className='left-side flex flex-col gap-5 w-1/4 text-start'>
-            <p>Filters: </p>
+    <div className='flex  flex-row min-h-screen overflow-hidden justify-between gap-5 p-10 bg-part mt-10'>
+        <div className='left-side flex flex-col gap-5 w-1/3 text-start p-7 rounded-xl bg-[#1F1F1F]'>
+            <p className='p-2'>Filters: </p>
             <input type="text" name="title" id="title" placeholder='Book Title: ' className='bg-part' />
             <input type="text" name="author" id="author" placeholder='The Author: ' className='bg-part' />
             <button>To apply</button>
@@ -24,14 +24,20 @@ const Recommendation = () => {
             </div>
         </div>
 
-        <div className='right-side p-3'>
-            <div>
-                <h2 className="text-3xl">Recommended</h2>
+        <div className='right-side w-full justify-start flex flex-col pl-7 pt-4 rounded-xl bg-[#1F1F1F] text-start'>
+            <div className='flex flex-col items-start'>
+                <h2 className="text-3xl self-start text-left pb-7">Recommended</h2>
                 <div className='flex flex-row gap-2'> 
                     <></>
                 </div>
             </div>
-            <div className='grid grid-cols-5 gap-3'></div>
+            <div className='grid grid-cols-5 gap-3'>
+                <div className='flex flex-col justify-start'>
+                    <img src="" alt="" className='rounded-xl' />
+                    <h2 className='font-md'>ad</h2>
+                    <p className='font-sm'>yazar</p>
+                </div>
+            </div>
         </div>
     </div>
   )
